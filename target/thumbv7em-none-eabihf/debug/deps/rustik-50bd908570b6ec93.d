c8d15fefe1066f3f2aa934c9e0bbca4573150f5c
Drahoxx <leo.chaigneau2@gmail.com>
2024-10-28 17:45:11 +0100

    Remove target folder

--- target/thumbv7em-none-eabihf/debug/deps/rustik-50bd908570b6ec93.d
+++
@@ -1,5 +0,0 @@
-/home/debian/rustiK/target/thumbv7em-none-eabihf/debug/deps/rustik-50bd908570b6ec93: src/main.rs
-
-/home/debian/rustiK/target/thumbv7em-none-eabihf/debug/deps/rustik-50bd908570b6ec93.d: src/main.rs
-
-src/main.rs:
--- target/thumbv7em-none-eabihf/debug/rustik.d
+++
@@ -1,1 +0,0 @@
-/home/debian/rustiK/target/thumbv7em-none-eabihf/debug/rustik: /home/debian/rustiK/src/main.rs
